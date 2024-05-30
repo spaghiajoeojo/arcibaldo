312187562514a4a1d2beb922753da93f0468b1d4
spaghiajoeojo <beccarolorenzo@gmail.com>
2024-05-30 12:47:32 +0200

    - fix compatibility issue

--- llm-agent/src/main.ts
+++ llm-agent/src/main.ts
@@ -16,7 +16,7 @@ const app = new ExpressBeans({
 });
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ type: ['application/x-www-form-urlencoded', 'text/html'] }));
 app.use((req: Request, _res: Response, next: NextFunction) => {
   req.setTimeout(500000, () => {
     // call back function is called when request timed out.
